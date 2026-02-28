1987f4d40f8db4fc592a3c1a5751bd3dc068e12a
Ernesto Afane <ernesto@97thfloor.com>
2026-02-27 22:31:53 -0600

    add profile_photo_url for the login functions

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -127,6 +127,8 @@ class AuthController extends Controller
                 'first_name' => $user->first_name,
                 'last_name' => $user->last_name,
                 'email' => $user->email,
+                'profile_photo_path' => $user->profile_photo_path,
+                'profile_photo_url' => $user->profile_photo_url,
                 'roles' => $user->roles->pluck('name'),
                 'organization' => $user->organization,
             ] : null,

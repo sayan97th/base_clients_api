7fef45150029ed18fec17ae1e253a052d1fa140a
Ernesto Afane <ernesto@97thfloor.com>
2026-03-13 10:14:41 -0600

    supervisor enviroments

--- supervisor/archlinux.sh
+++ supervisor/archlinux.sh
@@ -1,4 +1,4 @@
-sudo cp -f ./base_clients_api_local.conf /etc/supervisor.d/base_clients_api_local.conf
+sudo cp -f ./config/base_clients_api_local.conf /etc/supervisor.d/base_clients_api_local.conf
 sudo systemctl restart supervisord
 sudo supervisorctl reread
 sudo supervisorctl update

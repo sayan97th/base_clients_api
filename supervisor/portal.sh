ce2483639ddbe0e6714b17cd07bb2ee9e3e8385d
Ernesto Afane <ernesto@97thfloor.com>
2026-05-29 06:28:48 -0600

    portal config for base clients server

---
+++ supervisor/portal.sh
@@ -0,0 +1,6 @@
+sudo cp -f ./config/base_clients_api_portal.conf /etc/supervisor/conf.d/base_clients_api.conf
+sudo systemctl restart supervisor
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start base_clients_api:*
+sudo supervisorctl status
